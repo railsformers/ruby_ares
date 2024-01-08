9128f2bcbe0f14f010e5fb16ef6d51d5945bee44
Tomáš Král <tomas.kral@railsformers.com>
2024-01-08 12:04:54 +0100

    initial implementation

---
+++ lib/ruby_ares.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative "ruby_ares/version"
+
+module RubyAres
+  class Error < StandardError
+    attr_reader :status, :type, :response
+
+    def initialize(msg:, type:, status:, response:)
+      @status = status
+      @type = type
+      @response = response
+      super(msg)
+    end
+  end
+end
---
+++ lib/ruby_ares/subject.rb
@@ -0,0 +1,29 @@
+module RubyAres
+  class Subject
+    BASE_URL = 'https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty'.freeze
+
+    class << self
+      def get(ico, response_class = OpenStruct)
+        uri = URI("#{BASE_URL}/#{ico}")
+
+        response = Net::HTTP.get_response(uri)
+
+        case response
+        when Net::HTTPSuccess then
+          JSON.parse(response.body, object_class: response_class)
+        when Net::HTTPRedirection then
+          location = response['location']
+          warn "redirected to #{location}"
+          fetch(location, limit - 1)
+        else
+          raise RubyAres::Error.new(
+            msg: "Request failed with #{response.code}",
+            type: response.class.name,
+            status: response.code,
+            response: (JSON.parse(response.body) rescue response.body)
+          )
+        end
+      end
+    end
+  end
+end
